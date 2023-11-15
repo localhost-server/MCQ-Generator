4be5f110ac223f5e6f5029df2cc4ee9dcebbcdf3
localhost-server <99524394+localhost-server@users.noreply.github.com>
2023-11-15 16:52:04 +0530

    Delete pypdfcheck.py

--- pypdfcheck.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader
-
-# Opening the file using pypdf
-file = PdfReader("test.pdf")
-# Setting number of pages
-no_of_pages = len(file.pages)
-page=file.pages[0]
-text=page.extractText()
